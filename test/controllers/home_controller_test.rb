31d4674b01e2983263f0faae3500a2d55b4c8a9c
eford05 <89802740+eford05@users.noreply.github.com>
2024-09-01 12:29:41 -0500

    Change assert_response :success to 200, status

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,6 +3,6 @@ require "test_helper"
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get "/home"
-    assert_response :success
+    assert_response 200, status
   end
 end
