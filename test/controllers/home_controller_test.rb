ebb986b40027e3f271dd7f47921b885632318769
eford05 <89802740+eford05@users.noreply.github.com>
2024-09-01 12:06:26 -0500

    Modify test function assert_raise(NameError) home_index_url

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -3,6 +3,9 @@ require "test_helper"
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get home_index_url
+    assert_raises(NameError) do
+      home_index_url
+    end
     assert_response :success
   end
 end
